3bf20b3acb4f2d463c44af02d56820571d81a5e8
yuzq97 <47066501+yuzq97@users.noreply.github.com>
2021-08-20 16:19:26 +0800

    initial commit

---
+++ edit.py
@@ -0,0 +1,97 @@
+"""Edits latent codes with respect to given boundary.
+
+Basically, this file takes latent codes and a semantic boundary as inputs, and
+then shows how the image synthesis will change if the latent codes is moved
+towards the given boundary.
+
+"""
+
+import os.path
+import argparse
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+from models.model_settings import MODEL_POOL
+from models.stylegan_generator import StyleGANGenerator
+from utils.logger import setup_logger
+from utils.manipulator import linear_interpolate
+
+
+def parse_args():
+  """Parses arguments."""
+  parser = argparse.ArgumentParser(
+      description='Edit image synthesis with given semantic boundary.')
+  parser.add_argument('-m', '--model_name', type=str, required=True,
+                      choices=list(MODEL_POOL),
+                      help='Name of the model for generation. (required)')
+  parser.add_argument('-o', '--output_dir', type=str, required=True,
+                      help='Directory to save the output results. (required)')
+  parser.add_argument('-b', '--boundary_path', type=str, required=True,
+                      help='Path to the semantic boundary. (required)')
+  parser.add_argument('-i', '--input_latent_codes_path', type=str, default='',
+                      help='If specified, will load latent codes from given '
+                           'path instead of randomly sampling. (optional)')
+  parser.add_argument('-n', '--num', type=int, default=1,
+                      help='Number of images for editing. This field will be '
+                           'ignored if `input_latent_codes_path` is specified. '
+                           '(default: 1)')
+  parser.add_argument('--start_distance', type=float, default=-3.0,
+                      help='Start point for manipulation in latent space. '
+                           '(default: -3.0)')
+  parser.add_argument('--end_distance', type=float, default=3.0,
+                      help='End point for manipulation in latent space. '
+                           '(default: 3.0)')
+  parser.add_argument('--steps', type=int, default=10,
+                      help='Number of steps for image editing. (default: 10)')
+
+  return parser.parse_args()
+
+
+def main():
+  """Main function."""
+  args = parse_args()
+  logger = setup_logger(args.output_dir, logger_name='generate_data')
+
+  print('Initializing generator.\n')
+  model = StyleGANGenerator(args.model_name)
+
+  print('Preparing boundary.\n')
+  if not os.path.isfile(args.boundary_path):
+    raise ValueError(f'Boundary `{args.boundary_path}` does not exist!')
+  boundary = np.load(args.boundary_path)
+  np.save(os.path.join(args.output_dir, 'boundary.npy'), boundary)
+
+  print('Preparing latent codes.\n')
+  if os.path.isfile(args.input_latent_codes_path):
+    print(f'Load latent codes from `{args.input_latent_codes_path}`.\n')
+    latent_codes = np.load(args.input_latent_codes_path)
+    latent_codes = model.preprocess(latent_codes)
+  else:
+    print(f'Sample latent codes randomly.\n')
+    latent_codes = model.easy_sample(args.num)
+  np.save(os.path.join(args.output_dir, 'latent_codes.npy'), latent_codes)
+  total_num = latent_codes.shape[0]
+
+  print(f'Editing {total_num} samples.\n')
+  for sample_id in range(total_num):
+    interpolations = linear_interpolate(latent_codes[sample_id:sample_id + 1],
+                                        boundary,
+                                        start_distance=args.start_distance,
+                                        end_distance=args.end_distance,
+                                        steps=args.steps)
+    interpolation_id = 0
+    for interpolations_batch in model.get_batch_inputs(interpolations):
+      outputs = model.easy_synthesize(interpolations_batch)
+      for image in outputs['image']:
+        save_path = os.path.join(args.output_dir,
+                                 f'{sample_id:03d}_{interpolation_id:03d}.jpg')
+        cv2.imwrite(save_path, image[:, :, ::-1])
+        interpolation_id += 1
+    assert interpolation_id == args.steps
+    print(f'Finished sample {sample_id:3d}.\n')
+  print(f'Successfully edited {total_num} samples.\n')
+
+
+if __name__ == '__main__':
+  main()
---
+++ models/stylegan_generator.py
@@ -0,0 +1,184 @@
+"""Contains the generator class of StyleGAN.
+
+Basically, this class is derived from the `BaseGenerator` class defined in
+`base_generator.py`.
+"""
+
+import os
+import numpy as np
+
+import torch
+
+from . import model_settings
+from .stylegan_generator_model import StyleGANGeneratorModel
+from .base_generator import BaseGenerator
+
+__all__ = ['StyleGANGenerator']
+
+
+class StyleGANGenerator(BaseGenerator):
+
+  def __init__(self, model_name, logger=None):
+    self.truncation_psi = model_settings.STYLEGAN_TRUNCATION_PSI
+    self.truncation_layers = model_settings.STYLEGAN_TRUNCATION_LAYERS
+    self.randomize_noise = model_settings.STYLEGAN_RANDOMIZE_NOISE
+    self.model_specific_vars = ['truncation.truncation']
+    super().__init__(model_name, logger)
+    self.num_layers = (int(np.log2(self.resolution)) - 1) * 2
+    assert self.gan_type == 'stylegan'
+
+  def build(self):
+    self.check_attr('w_space_dim')
+    self.check_attr('fused_scale')
+    self.model = StyleGANGeneratorModel(
+        resolution=self.resolution,
+        w_space_dim=self.w_space_dim,
+        fused_scale=self.fused_scale,
+        output_channels=self.output_channels,
+        truncation_psi=self.truncation_psi,
+        truncation_layers=self.truncation_layers,
+        randomize_noise=self.randomize_noise)
+
+  def load(self):
+    print(f'Loading pytorch model from `{self.model_path}`.')
+    state_dict = torch.load(self.model_path)
+    for var_name in self.model_specific_vars:
+      state_dict[var_name] = self.model.state_dict()[var_name]
+    self.model.load_state_dict(state_dict)
+    print(f'Successfully loaded!')
+    # self.lod = self.model.synthesis.lod.to(self.cpu_device).tolist()
+    # print(f'  `lod` of the loaded model is {self.lod}.')
+
+  def convert_tf_model(self, test_num=10):
+    import sys
+    import pickle
+    import tensorflow as tf
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+    sys.path.append(model_settings.BASE_DIR + '/stylegan_tf_official')
+
+    print(f'Loading tensorflow model from `{self.tf_model_path}`.\n')
+    tf.InteractiveSession()
+    with open(self.tf_model_path, 'rb') as f:
+      _, _, tf_model = pickle.load(f)
+    print(f'Successfully loaded!\n')
+
+    print(f'Converting tensorflow model to pytorch version.\n')
+    tf_vars = dict(tf_model.__getstate__()['variables'])
+    tf_vars.update(
+        dict(tf_model.components.mapping.__getstate__()['variables']))
+    tf_vars.update(
+        dict(tf_model.components.synthesis.__getstate__()['variables']))
+    state_dict = self.model.state_dict()
+    for pth_var_name, tf_var_name in self.model.pth_to_tf_var_mapping.items():
+      if 'ToRGB_lod' in tf_var_name:
+        lod = int(tf_var_name[len('ToRGB_lod')])
+        lod_shift = 10 - int(np.log2(self.resolution))
+        tf_var_name = tf_var_name.replace(f'{lod}', f'{lod - lod_shift}')
+      if tf_var_name not in tf_vars:
+        print(f'Variable `{tf_var_name}` does not exist in '
+                          f'tensorflow model.\n')
+        continue
+      print(f'Converting `{tf_var_name}` to `{pth_var_name}`.\n')
+      var = torch.from_numpy(np.array(tf_vars[tf_var_name]))
+      if 'weight' in pth_var_name:
+        if 'dense' in pth_var_name:
+          var = var.permute(1, 0)
+        elif 'conv' in pth_var_name:
+          var = var.permute(3, 2, 0, 1)
+      state_dict[pth_var_name] = var
+    print(f'Successfully converted!\n')
+
+    print(f'Saving pytorch model to `{self.model_path}`.\n')
+    for var_name in self.model_specific_vars:
+      del state_dict[var_name]
+    torch.save(state_dict, self.model_path)
+    print(f'Successfully saved!\n')
+
+    self.load()
+
+  def sample(self, num):
+    """Samples latent codes randomly.
+
+    Args:
+      num: Number of latent codes to sample. Should be positive.
+
+    Returns:
+      A `numpy.ndarray` as sampled latend codes.
+
+    """
+    latent_codes = np.random.randn(num, self.latent_space_dim)
+
+    return latent_codes.astype(np.float32)
+
+  def preprocess(self, latent_codes):
+    """Preprocesses the input latent code if needed.
+
+    Args:
+      latent_codes: The input latent codes for preprocessing.
+
+    Returns:
+      The preprocessed latent codes which can be used as final input for the
+        generator.
+    """
+    if not isinstance(latent_codes, np.ndarray):
+      raise ValueError(f'Latent codes should be with type `numpy.ndarray`!')
+
+    latent_codes = latent_codes.reshape(-1, self.latent_space_dim)
+    norm = np.linalg.norm(latent_codes, axis=1, keepdims=True)
+    latent_codes = latent_codes / norm * np.sqrt(self.latent_space_dim)
+
+    return latent_codes.astype(np.float32)
+
+  def easy_sample(self, num):
+    return self.preprocess(self.sample(num))
+
+  def synthesize(self,
+                 latent_codes,
+                 generate_style=False,
+                 generate_image=True):
+    """Synthesizes images with given latent codes.
+
+    One can choose whether to generate the layer-wise style codes.
+
+    Args:
+      latent_codes: Input latent codes for image synthesis.
+      generate_style: Whether to generate the layer-wise style codes. (default:
+        False)
+      generate_image: Whether to generate the final image synthesis. (default:
+        True)
+
+    Returns:
+      A dictionary whose values are raw outputs from the generator.
+    """
+    if not isinstance(latent_codes, np.ndarray):
+      raise ValueError(f'Latent codes should be with type `numpy.ndarray`!')
+
+    results = {}
+
+    latent_codes_shape = latent_codes.shape
+
+    if not (len(latent_codes_shape) == 2 and
+              latent_codes_shape[0] <= self.batch_size and
+              latent_codes_shape[1] == self.latent_space_dim):
+      raise ValueError(f'Latent_codes should be with shape [batch_size, '
+                         f'latent_space_dim], where `batch_size` no larger '
+                         f'than {self.batch_size}, and `latent_space_dim` '
+                         f'equal to {self.latent_space_dim}!\n'
+                         f'But {latent_codes_shape} received!')
+    zs = torch.from_numpy(latent_codes).type(torch.FloatTensor)
+    zs = zs.to(self.run_device)
+    ws = self.model.mapping(zs)
+    wps = self.model.truncation(ws)
+    results['z'] = latent_codes
+
+    if generate_style:
+      for i in range(self.num_layers):
+        style = self.model.synthesis.__getattr__(
+            f'layer{i}').epilogue.style_mod.dense(wps[:, i, :])
+        results[f'style{i:02d}'] = self.get_value(style)
+
+    if generate_image:
+      images = self.model.synthesis(wps)
+      results['image'] = self.get_value(images)
+
+    return results
